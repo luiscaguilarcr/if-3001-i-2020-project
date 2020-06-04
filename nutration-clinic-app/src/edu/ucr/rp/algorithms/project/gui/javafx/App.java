1174937c55dcc44e696e2ba08d82720dbcdab789
Luis Carlos Aguilar <luis90101045@gmail.com>
2020-06-03 23:18:32 -0600

    Iniciar proyecto

---
+++ nutration-clinic-app/src/edu/ucr/rp/algorithms/project/gui/javafx/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ucr.rp.algorithms.project.gui.javafx;
+
+/**
+ *
+ * @author luis1
+ */
+public class App {
+    public static void main(String...args){
+        AppStage appStage = new AppStage();
+    }
+}
