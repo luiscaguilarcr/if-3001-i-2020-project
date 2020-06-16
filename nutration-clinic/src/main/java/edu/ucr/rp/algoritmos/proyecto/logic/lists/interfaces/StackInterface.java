c3e430aebdb9dfa2323d98f49de0bffdcf856d67
Maikel06 <sanchezrojasmaikel@gmail.com>
2020-06-16 16:07:58 -0600

    Implementacion de metodos pila

--- nutration-clinic/src/main/java/edu/ucr/rp/algoritmos/proyecto/logic/lists/interfaces/StackInterface.java
+++ nutration-clinic/src/main/java/edu/ucr/rp/algoritmos/proyecto/logic/lists/interfaces/StackInterface.java
@@ -1,4 +1,17 @@
 package edu.ucr.rp.algoritmos.proyecto.logic.lists.interfaces;
 
 public interface StackInterface {
+    
+   public void push(int x);
+
+    public Object pop();
+
+    public Object peek();
+
+    public int size();
+
+    public boolean isFull();
+
+    public boolean isEmpty();
+
 }
