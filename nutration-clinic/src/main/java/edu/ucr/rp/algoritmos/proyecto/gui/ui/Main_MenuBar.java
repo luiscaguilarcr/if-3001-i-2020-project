d7e57c78f0ab60583d6dbad59625829528f7918a
Luis Carlos Aguilar <luiscarlos.aguilar@ucr.ac.cr>
2020-06-29 16:47:56 -0600

    Update: exercises icon

--- nutration-clinic/src/main/java/edu/ucr/rp/algoritmos/proyecto/gui/ui/Main_MenuBar.java
+++ nutration-clinic/src/main/java/edu/ucr/rp/algoritmos/proyecto/gui/ui/Main_MenuBar.java
@@ -129,7 +129,7 @@ public class Main_MenuBar implements PaneViewer {
         exit_MenuItem.setOnAction((event) -> Platform.exit());
         exit_MenuItem.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
 
-        config_Menu.getItems().addAll(changePassword_MenuItem, personalProgress_MenuItem, guide_MenuItem, logOut_MenuItem, cleanApp_MenuItem, exit_MenuItem);
+        config_Menu.getItems().addAll(changePassword_MenuItem, personalProgress_MenuItem, guide_MenuItem, exercise_MenuItem, logOut_MenuItem, cleanApp_MenuItem, exit_MenuItem);
 
         ////////////////////////////////////////////////////////////////////////// Menu "ADMIN"
         admin_Menu = new Menu("ADMIN");
